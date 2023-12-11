b5878b6de54f50e8b6094cc0c7772aee2ca5f287
cueltschey <chaseuelt@gmail.com>
2023-12-11 16:01:34 -0600

    added training data

--- neuron.py
+++ neuron.py
@@ -1,10 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+np.random.seed(0)
 
-
-X = [[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    Y = np.zeros((points*classes, 2))
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)
+        t = np,linspace(class_number * 4, (class_number+1) * 4, points) + np.random.randn(points) * 0.2
+        X[ix] = np.c_[r*]
 
 
 
@@ -14,6 +20,10 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
 
 layer1 = Layer_Dense(4, 5)
